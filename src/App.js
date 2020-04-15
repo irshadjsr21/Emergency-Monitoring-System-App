41b32507d0f86365fa1fd7c5ceeafe5dcc6b3e67
Habeeba Naaz <habeebanaaz1@gmail.com>
2020-04-15 02:13:34 -0400

    Added Login Screens

--- src/App.js
+++ src/App.js
@@ -3,17 +3,26 @@ import './App.css';
 import AdminSignup from './components/register/AdminSignup';
 import RegisterHospital from './components/register/RegisterHospital';
 import RegisterParamedic from './components/register/RegisterParamedic';
+import AdminLogin from './components/Login/AdminLogin';
+import LoginParamedic from './components/Login/LoginParamedic';
+import LoginHospital from './components/Login/LoginHospital';
 
 function App() {
   return (
-      <div>
+    <div>
       <AdminSignup />
       <br />
       <RegisterHospital />
       <br />
       <RegisterParamedic />
-      </div>
-      );
+      <br />
+      <AdminLogin />
+      <br />
+      <LoginParamedic />
+      <br />
+      <LoginHospital />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Login/LoginHospital.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react'
+import Col from 'react-bootstrap/Col'
+import Form from 'react-bootstrap/Form'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import admin from '../../images/admin.jpeg'
+
+function LoginHospital() {
+    const [validated, setValidated] = useState(false);
+
+    const handleSubmit = (event) => {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+        setValidated(true);
+    };
+
+    return (
+        <Card fluid bg="light" border="success" style={{ backgroundImage: `url(${admin})`, opacity: 0.8, color: 'red', textAlign: 'center', justifyContent: 'center' }}>
+            <br />
+            <h2>Hospital Login</h2>
+            <Form noValidate validated={validated} onSubmit={handleSubmit} >
+
+                <Form.Row className="justify-content-md-center">
+                    <Form.Group as={Col} md="4" controlId="validationCustomEmail">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control
+                            type="email"
+                            required
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            Please provide a valid email.
+              </Form.Control.Feedback>
+                    </Form.Group>
+                </Form.Row>
+
+
+                <Form.Row className="justify-content-md-center">
+                    <Form.Group as={Col} md="4" controlId="validationCustomPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control
+                            type="password"
+                            required
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            Please provide a valid password.
+              </Form.Control.Feedback>
+                    </Form.Group>
+                </Form.Row>
+
+                <Button type="submit">Login Branch</Button>
+            </Form>
+            <br />
+        </Card>
+
+    );
+
+}
+
+export default LoginHospital
