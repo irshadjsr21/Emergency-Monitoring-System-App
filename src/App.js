6454f535f41ad27cf4b0161713d2044dc91c773a
Humza Hasan <humzahasan2101@gmail.com>
2020-04-16 00:28:46 +0530

    Dashboard Added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
 import React from 'react';
+import { BrowserRouter as Router , Switch , Route } from 'react-router-dom'
 import './App.css';
+import AdminDashboard from './components/AdminDashboard';
 import AdminSignup from './components/register/AdminSignup';
 import RegisterHospital from './components/register/RegisterHospital';
 import RegisterParamedic from './components/register/RegisterParamedic';
+import HomePage from './components/HomePage'
 import AdminLogin from './components/Login/AdminLogin';
 import LoginParamedic from './components/Login/LoginParamedic';
 import LoginHospital from './components/Login/LoginHospital';
 
 function App() {
   return (
-    <div>
-      <AdminSignup />
-      <br />
-      <RegisterHospital />
-      <br />
-      <RegisterParamedic />
-      <br />
-      <AdminLogin />
-      <br />
-      <LoginParamedic />
-      <br />
-      <LoginHospital />
-    </div>
+    <Router>
+    
+      
+      <Switch>
+        <Route exact path = "/" component={HomePage} />
+        <Route exact path = "/adminregister" component={AdminSignup} />
+        <Route exact path = "/hospitalregister" component={RegisterHospital} />
+        <Route exact path = "/paramedicregister" component={RegisterParamedic} />
+        <Route exact path = "/adminlogin" component={AdminLogin} />
+        <Route exact path = "/admindashboard" component={AdminDashboard} />
+        <Route exact path = "/hospitallogin" component={LoginHospital} />
+        <Route exact path = "/paramediclogin" component={LoginParamedic} />
+
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/AdminDashboard.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+//import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card'
+import CardGroup from 'react-bootstrap/CardGroup'
+
+function AdminDashboard() {
+    return (
+        <div>
+            <h1 >Admin Dashboard</h1>
+            <br />
+            <CardGroup>
+  <Card className="justify-content-md-center">
+    <Card.Body>
+      <Card.Title>Hospital Registration</Card.Title>
+      <Card.Text>
+        Dear Admin, you can now add all your network hospitals to our system and let the words of your Paramedic be heard instantaneously
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+    <Link to = '/hospitalregister' variant="primary">Hospital Registration</Link>
+    </Card.Footer>
+  </Card>
+  <Card>
+    <Card.Body>
+      <Card.Title>Paramedic Registration</Card.Title>
+      <Card.Text>
+        Dear Admin, you can add all the heros of your paramedic department who are busy out there saving lives for faster information transfer to your networked hospitals
+        content.
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+    <Link to="/paramedicregister" variant="danger">Paramedic Registration</Link>
+    </Card.Footer>
+  </Card>
+  </CardGroup>
+          </div>
+    )
+}
+
+export default AdminDashboard
