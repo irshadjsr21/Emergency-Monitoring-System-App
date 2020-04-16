b0ffafbe2d8a0013bbcd183514aacb2fd358298c
Irshad Ansari <irshadjsr21@gmail.com>
2020-04-16 22:36:11 +0530

    [Fix] Fixed build errors

--- src/components/ManualInput.js
+++ src/components/ManualInput.js
@@ -1,12 +1,11 @@
-import React, {useState,Component} from 'react'
-import Col from 'react-bootstrap/Col'
-import Form from 'react-bootstrap/Form'
-import Card from 'react-bootstrap/Card'
-import Button from 'react-bootstrap/Button'
-import admin from '../../images/admin.jpeg'
+import React, { Component } from 'react';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import admin from '../images/admin.jpeg';
 
-export default class ManualInput extends Component{
-
+export default class ManualInput extends Component {
   constructor(props) {
     super(props);
 
@@ -19,165 +18,176 @@ export default class ManualInput extends Component{
     this.onSubmit = this.onSubmit.bind(this);
 
     this.state = {
-        name:"",
-        number:"",
-        enumber:"",
-        readings:"",
-        nature:"",
-        condition:""
-    }
+      name: '',
+      number: '',
+      enumber: '',
+      readings: '',
+      nature: '',
+      condition: '',
+    };
   }
 
   onChangeName(e) {
     this.setState({
-      name: e.target.value
-    })
+      name: e.target.value,
+    });
   }
 
   onChangeNumber(e) {
     this.setState({
-      number: e.target.value
-    })
+      number: e.target.value,
+    });
   }
 
   onChangeEnumber(e) {
     this.setState({
-      enumber: e.target.value
-    })
+      enumber: e.target.value,
+    });
   }
 
   onChangeReadings(e) {
     this.setState({
-      readings: e.target.value
-    })
+      readings: e.target.value,
+    });
   }
 
   onChangeNature(e) {
     this.setState({
-      nature: e.target.value
-    })
+      nature: e.target.value,
+    });
   }
 
   onChangeCondition(e) {
     this.setState({
-      condition: e.target.value
-    })
+      condition: e.target.value,
+    });
   }
 
   onSubmit(e) {
     e.preventDefault();
 
     const form = {
-        name:this.state.name,
-        number:this.state.number,
-        enumber:this.state.enumber,
-        readings:this.state.readings,
-        nature:this.state.nature,
-        condition:this.state.condition
-    }
+      name: this.state.name,
+      number: this.state.number,
+      enumber: this.state.enumber,
+      readings: this.state.readings,
+      nature: this.state.nature,
+      condition: this.state.condition,
+    };
 
     console.log(form);
-
- 
   }
 
-  render(){
+  render() {
     return (
-        <Card border="info" style={{ backgroundImage: `url(${admin})`, opacity: 0.8, color: 'black', textAlign: 'center', justifyContent: 'center' }}>
+      <Card
+        border="info"
+        style={{
+          backgroundImage: `url(${admin})`,
+          opacity: 0.8,
+          color: 'black',
+          textAlign: 'center',
+          justifyContent: 'center',
+        }}
+      >
         <br />
         <h2>Manual Input</h2>
 
-        <Form noValidate validated={validated} onSubmit={handleSubmit} >
+        <Form onSubmit={this.onSubmit}>
+          <Form.Row className="justify-content-md-center">
+            <Form.Group as={Col} md="4" controlId="validationPatientName">
+              <Form.Label>Patient Name</Form.Label>
+              <Form.Control
+                required
+                type="text"
+                value={this.state.name}
+                onChange={this.onChangeName}
+              />
+              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            </Form.Group>
+          </Form.Row>
 
-            <Form.Row className="justify-content-md-center">
-                <Form.Group as={Col} md="4" controlId="validationPatientName">
-                    <Form.Label>Patient Name</Form.Label>
-                    <Form.Control
-                        required
-                        type="text"
-                        value={this.state.name}
-                        onChange={this.onChangeName}
-                    />
-                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                </Form.Group>
-            </Form.Row>
+          <Form.Row className="justify-content-md-center">
+            <Form.Group as={Col} md="4" controlId="validationContactNumber">
+              <Form.Label>Contact Number</Form.Label>
+              <Form.Control
+                required
+                value={this.state.number}
+                onChange={this.onChangeNumber}
+              />
+              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            </Form.Group>
+          </Form.Row>
 
-            <Form.Row className="justify-content-md-center">
-                <Form.Group as={Col} md="4" controlId="validationContactNumber">
-                    <Form.Label>Contact Number</Form.Label>
-                    <Form.Control
-                        required
-                        value={this.state.number}
-              onChange={this.onChangeNumber}
-                    />
-                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                </Form.Group>
-            </Form.Row>
+          <Form.Row className="justify-content-md-center">
+            <Form.Group
+              as={Col}
+              md="4"
+              controlId="validationEmergencyContactNumber"
+            >
+              <Form.Label>Emergency Contact Number</Form.Label>
+              <Form.Control
+                required
+                value={this.state.enumber}
+                onChange={this.onChangeEnumber}
+              />
+              <Form.Control.Feedback type="invalid">
+                Please provide a valid Contact Number.
+              </Form.Control.Feedback>
+            </Form.Group>
+          </Form.Row>
 
-            <Form.Row className="justify-content-md-center">
-                <Form.Group as={Col} md="4" controlId="validationEmergencyContactNumber">
-                    <Form.Label>Emergency Contact Number</Form.Label>
-                    <Form.Control
-                        required
-                        value={this.state.enumber}
-              onChange={this.onChangeEnumber}
-                    />
-                    <Form.Control.Feedback type="invalid">
-                        Please provide a valid Contact Number.
-          </Form.Control.Feedback>
-                </Form.Group>
-            </Form.Row>
+          <Form.Row className="justify-content-md-center">
+            <Form.Group as={Col} md="4" controlId="validationMonitorReadings">
+              <Form.Label>Monitor Readings</Form.Label>
+              <Form.Control
+                required
+                value={this.state.readings}
+                onChange={this.onChangeReadings}
+              />
+              <Form.Control.Feedback type="invalid">
+                Please provide a valid readings.
+              </Form.Control.Feedback>
+            </Form.Group>
+          </Form.Row>
 
+          <Form.Row className="justify-content-md-center">
+            <Form.Group as={Col} md="4" controlId="validationNatureOfAccident">
+              <Form.Label>Nature of Accident</Form.Label>
+              <Form.Control
+                required
+                value={this.state.accnature}
+                onChange={this.onChangeAccNature}
+              />
+              <Form.Control.Feedback type="invalid">
+                Please provide a valid details.
+              </Form.Control.Feedback>
+            </Form.Group>
+          </Form.Row>
 
-            <Form.Row className="justify-content-md-center">
-                <Form.Group as={Col} md="4" controlId="validationMonitorReadings">
-                    <Form.Label>Monitor Readings</Form.Label>
-                    <Form.Control
-                        required
-                        value={this.state.readings}
-              onChange={this.onChangeReadings}
-                    />
-                    <Form.Control.Feedback type="invalid">
-                        Please provide a valid readings.
-          </Form.Control.Feedback>
-                </Form.Group>
-            </Form.Row>
+          <Form.Row className="justify-content-md-center">
+            <Form.Group
+              as={Col}
+              md="4"
+              controlId="validationSeverityOfCondition"
+            >
+              <Form.Label>Severity of Condition</Form.Label>
+              <Form.Control
+                required
+                value={this.state.condition}
+                onChange={this.onChangeCondition}
+              />
+              <Form.Control.Feedback type="invalid">
+                Please provide a valid details.
+              </Form.Control.Feedback>
+            </Form.Group>
+          </Form.Row>
 
-            <Form.Row className="justify-content-md-center">
-                <Form.Group as={Col} md="4" controlId="validationNatureOfAccident">
-                    <Form.Label>Nature of Accident</Form.Label>
-                    <Form.Control
-                        required
-                        value={this.state.accnature}
-              onChange={this.onChangeAccNature}
-                    />
-                    <Form.Control.Feedback type="invalid">
-                        Please provide a valid details.
-          </Form.Control.Feedback>
-                </Form.Group>
-            </Form.Row>
-
-            <Form.Row className="justify-content-md-center">
-                <Form.Group as={Col} md="4" controlId="validationSeverityOfCondition">
-                    <Form.Label>Severity of Condition</Form.Label>
-                    <Form.Control
-                        required
-                        value={this.state.condition}
-              onChange={this.onChangeCondition}
-                    />
-                    <Form.Control.Feedback type="invalid">
-                        Please provide a valid details.
-          </Form.Control.Feedback>
-                </Form.Group>
-            </Form.Row>
-
-            <Button type="submit">Submit</Button>
-            <br />
+          <Button type="submit">Submit</Button>
+          <br />
         </Form>
         <br />
-    </Card>
-
-       );
+      </Card>
+    );
   }
 }
-
--- src/components/QuickActions.js
+++ src/components/QuickActions.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-
-
+import { Link } from 'react-router-dom';
 
 export default class QuickActions extends Component {
   render() {
     return (
-    <div>
-       <Link to={"/manualinput" }>Start Manual Input</Link><Link to={"/video" }>Emergency Video Sharing</Link>
-    </div>
-    )
+      <div>
+        <Link to={'/manualinput'}>Start Manual Input</Link>
+        <Link to={'/video'}>Emergency Video Sharing</Link>
+      </div>
+    );
   }
 }
