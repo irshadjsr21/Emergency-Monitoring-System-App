26198b983326c9ab981f7e5583a8dea406d768c3
Harshak777 <Harshak777@users.noreply.github.com>
2020-04-16 12:33:19 +0530

    added axios in admin/signup
    I usually use state variables to store the data from the form and send it to the server!

--- src/components/register/AdminSignup.js
+++ src/components/register/AdminSignup.js
@@ -1,96 +1,144 @@
-import React, {useState} from 'react'
+import React, {useState,Component} from 'react'
+import axios from 'axios';
 import Col from 'react-bootstrap/Col'
 import Form from 'react-bootstrap/Form'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
 import admin from '../../images/admin.jpeg'
 
-function AdminSignup() {
+export default class CreateExercise extends Component{
 
-    const [validated, setValidated] = useState(false);
-    
-    const handleSubmit = (event) => {
-        const form = event.currentTarget;
-        if (form.checkValidity() === false) {
-        event.preventDefault();
-        event.stopPropagation();
-        }
-    
-        setValidated(true);
-    };
+  constructor(props) {
+    super(props);
 
-    return (
-        <Card  border="info" style={{backgroundImage:`url(${admin})`,opacity: 0.8,color:'black',textAlign:'center', justifyContent: 'center'}}>
-        <br />
-        <h2>Admin SignUp</h2>
+    this.onChangeName = this.onChangeName.bind(this);
+    this.onChangeEmail = this.onChangeEmail.bind(this);
+    this.onChangePassword = this.onChangePassword.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
 
-        <Form noValidate validated={validated} onSubmit={handleSubmit} >
-        
-        <Form.Row className="justify-content-md-center">
-          <Form.Group as={Col} md="4" controlId="validationAdminName">
-            <Form.Label>Hospital Name</Form.Label>
-            <Form.Control
-              required
-              type="text"
-            />
-            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-          </Form.Group>
-        </Form.Row>
-
-        <Form.Row className="justify-content-md-center">
-          <Form.Group as={Col} md="4" controlId="validationAdminReg">
-            <Form.Label>Hospital Registration Number</Form.Label>
-            <Form.Control
-              required
-              type="text"
-            />
-            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-          </Form.Group>
-        </Form.Row>
-
-        <Form.Row className="justify-content-md-center">
-          <Form.Group as={Col} md="4" controlId="validationAdminEmail">
-            <Form.Label>Email</Form.Label>
-              <Form.Control
-                type="email"
-                required
-              />
-              <Form.Control.Feedback type="invalid">
-                Please provide a valid email.
-              </Form.Control.Feedback>  
-          </Form.Group>
-        </Form.Row>
-        
-        
-        <Form.Row className="justify-content-md-center">
-          <Form.Group as={Col} md="4" controlId="validationAdminPassword">
-            <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="password"
-                required
-              />
-              <Form.Control.Feedback type="invalid">
-                Please provide a valid password.
-              </Form.Control.Feedback>  
-          </Form.Group>
-        </Form.Row>
-
-        
-        <Form.Group>
-          <Form.Check
-            required
-            label="Agree to terms and conditions"
-            feedback="You must agree before submitting."
-          />
-        </Form.Group>
-        <Button type="submit">Admin SignUp</Button>
-        <br />
-      </Form>
-      <br />
-      </Card>
-  
-         );
+    this.state = {
+      name: '',
+      password: '',
+      email: ''
     }
-          
-export default AdminSignup
+  }
+
+
+  onChangeName(e) {
+    this.setState({
+      name: e.target.value
+    })
+  }
+
+  onChangeEmail(e) {
+    this.setState({
+      email: e.target.value
+    })
+  }
+
+  onChangePassword(e) {
+    this.setState({
+      password: e.target.value
+    })
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const form = {
+      name: this.state.name,
+      email: this.state.email,
+      password: this.state.password,
+    }
+
+    console.log(form);
+
+    axios.post('http://localhost:5000/admin/signup', form)
+      .then(res => console.log(res.data));
+
+ 
+  }
+
+  render(){
+    return (
+      <Card  border="info" style={{backgroundImage:`url(${admin})`,opacity: 0.8,color:'black',textAlign:'center', justifyContent: 'center'}}>
+      <br />
+      <h2>Admin SignUp</h2>
+
+      <Form onSubmit={this.onSubmit} >
+      
+      <Form.Row className="justify-content-md-center">
+        <Form.Group as={Col} md="4" controlId="validationAdminName">
+          <Form.Label>Hospital Name</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            value={this.state.name}
+            onChange={this.onChangeName}
+          />
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        </Form.Group>
+      </Form.Row>
+
+{/*      <Form.Row className="justify-content-md-center">
+        <Form.Group as={Col} md="4" controlId="validationAdminReg">
+          <Form.Label>Hospital Registration Number</Form.Label>
+          <Form.Control
+            required
+            type="text"
+          />
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        </Form.Group>
+    </Form.Row>*/}
+
+      <Form.Row className="justify-content-md-center">
+        <Form.Group as={Col} md="4" controlId="validationAdminEmail">
+          <Form.Label>Email</Form.Label>
+            <Form.Control
+              type="email"
+              required
+              value={this.state.email}
+              onChange={this.onChangeEmail}
+            />
+            <Form.Control.Feedback type="invalid">
+              Please provide a valid email.
+            </Form.Control.Feedback>  
+        </Form.Group>
+      </Form.Row>
+      
+      
+      <Form.Row className="justify-content-md-center">
+        <Form.Group as={Col} md="4" controlId="validationAdminPassword">
+          <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              required
+              value={this.state.password}
+              onChange={this.onChangePassword}
+            />
+            <Form.Control.Feedback type="invalid">
+              Please provide a valid password.
+            </Form.Control.Feedback>  
+        </Form.Group>
+      </Form.Row>
+
+      
+      <Form.Group>
+        <Form.Check
+          required
+          label="Agree to terms and conditions"
+          feedback="You must agree before submitting."
+        />
+      </Form.Group>
+      <Button type="submit">Admin SignUp</Button>
+      <br />
+    </Form>
+    <br />
+    </Card>
+
+       );
+  }
+}
+
+
 
