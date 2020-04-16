f46feee8890cfad3ae9f8ad64c7ed4d23074471c
Harshak777 <Harshak777@users.noreply.github.com>
2020-04-16 13:02:20 +0530

    updated for admin login

--- src/components/Login/AdminLogin.js
+++ src/components/Login/AdminLogin.js
@@ -1,73 +1,102 @@
-import React, { useState } from 'react'
+import React, {useState,Component} from 'react'
+import axios from 'axios';
 import Col from 'react-bootstrap/Col'
 import Form from 'react-bootstrap/Form'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
 import admin from '../../images/admin.jpeg'
 
-function AdminLogin() {
+export default class AdminLogin extends Component{
 
-    const [validated, setValidated] = useState(false);
+  constructor(props) {
+    super(props);
 
-    const handleSubmit = (event) => {
-        const form = event.currentTarget;
-        if (form.checkValidity() === false) {
-            event.preventDefault();
-            event.stopPropagation();
-        }
+    this.onChangeEmail = this.onChangeEmail.bind(this);
+    this.onChangePassword = this.onChangePassword.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
 
-        setValidated(true);
-    };
+    this.state = {
+      password: '',
+      email: ''
+    }
+  }
 
+  onChangeEmail(e) {
+    this.setState({
+      email: e.target.value
+    })
+  }
+
+  onChangePassword(e) {
+    this.setState({
+      password: e.target.value
+    })
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const form = {
+      email: this.state.email,
+      password: this.state.password,
+    }
+
+    console.log(form);
+
+    axios.post('http://localhost:5000/admin/login', form)
+      .then(res => console.log(res.data));
+
+ 
+  }
+
+  render(){
     return (
-        <Card border="info" style={{ backgroundImage: `url(${admin})`, opacity: 0.8, color: 'black', textAlign: 'center', justifyContent: 'center' }}>
-            <br />
-            <h2>Admin Login</h2>
+      <Card  border="info" style={{backgroundImage:`url(${admin})`,opacity: 0.8,color:'black',textAlign:'center', justifyContent: 'center'}}>
+      <br />
+      <h2>Admin SignUp</h2>
 
-            <Form noValidate validated={validated} onSubmit={handleSubmit} >
+      <Form onSubmit={this.onSubmit} >
 
-                <Form.Row className="justify-content-md-center">
-                    <Form.Group as={Col} md="4" controlId="validationAdminEmail">
-                        <Form.Label>Email</Form.Label>
-                        <Form.Control
-                            type="email"
-                            required
-                        />
-                        <Form.Control.Feedback type="invalid">
-                            Please provide a valid email.
-              </Form.Control.Feedback>
-                    </Form.Group>
-                </Form.Row>
+      <Form.Row className="justify-content-md-center">
+        <Form.Group as={Col} md="4" controlId="validationAdminEmail">
+          <Form.Label>Email</Form.Label>
+            <Form.Control
+              type="email"
+              required
+              value={this.state.email}
+              onChange={this.onChangeEmail}
+            />
+            <Form.Control.Feedback type="invalid">
+              Please provide a valid email.
+            </Form.Control.Feedback>  
+        </Form.Group>
+      </Form.Row>
+      
+      
+      <Form.Row className="justify-content-md-center">
+        <Form.Group as={Col} md="4" controlId="validationAdminPassword">
+          <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              required
+              value={this.state.password}
+              onChange={this.onChangePassword}
+            />
+            <Form.Control.Feedback type="invalid">
+              Please provide a valid password.
+            </Form.Control.Feedback>  
+        </Form.Group>
+      </Form.Row>
 
+      <Button type="submit">Admin SignUp</Button>
+      <br />
+    </Form>
+    <br />
+    </Card>
 
-                <Form.Row className="justify-content-md-center">
-                    <Form.Group as={Col} md="4" controlId="validationAdminPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control
-                            type="password"
-                            required
-                        />
-                        <Form.Control.Feedback type="invalid">
-                            Please provide a valid password.
-              </Form.Control.Feedback>
-                    </Form.Group>
-                </Form.Row>
-
-
-                <Form.Group>
-                    <Form.Check
-                        required
-                        label="Agree to terms and conditions"
-                        feedback="You must agree before submitting."
-                    />
-                </Form.Group>
-                <Button type="submit">Login Admin</Button>
-                <br />
-            </Form>
-            <br />
-        </Card>
-
-    );
+       );
+  }
 }
 
-export default AdminLogin
+
+
