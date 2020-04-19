c5d07e05822577b62f36ed6aa4c625c5ca40e30c
Humza Hasan <humzahasan2101@gmail.com>
2020-04-19 22:59:14 +0530

    UI with previous alerts (#15)

--- src/components/HospitalDashboard.js
+++ src/components/HospitalDashboard.js
@@ -6,29 +6,26 @@ import Button from 'react-bootstrap/Button'
 let socket;
 
 const HospitalDashboard = () => {
-  const [name, setName] = useState('');
+  /*const [name, setName] = useState('');
   const [number, setNumber] = useState('');
   const [enumber, setEnumber] = useState('');
   const [readings, setReadings] = useState('');
   const [nature, setNature] = useState('');
-  const [condition, setCondition] = useState('');
+  const [condition, setCondition] = useState('');*/
+  const [datas,setDatas] = useState([]);
 
-  const ENDPOINT = 'http://localhost:4000';
+  const ENDPOINT = 'http://localhost:4000/';
 
   useEffect(() => {
 
     socket = io(ENDPOINT);
 
   }, [ENDPOINT]);
-  
+
   useEffect(() => {
     socket.on('chat', data => {
-      setName(data.name);
-      setNumber(data.number);
-      setEnumber(data.enumber);
-      setReadings(data.readings);
-      setNature(data.nature);
-      setCondition(data.condition);
+      console.log(data)
+      setDatas(datas=>[...datas,data])
     });
 }, []);
 
@@ -41,23 +38,29 @@ const HospitalDashboard = () => {
   }
 
   return (
-<div>
-<Alert variant="danger">
-  <Alert.Heading>Incoming Patient Details</Alert.Heading>
-  
-  <p>Name : {name}</p>
-    <p>Contact Number : {number}</p>
-    <p>Emergency Contact Number : {enumber}</p>
-    <p>Monitor Reading : {readings}</p>
-    <p>Nature of Emergency : {nature}</p>
-    <p>Severity of Emergency : {condition}</p>
-  
+<div className="justify-content-md-center" style={{ color: 'black', textAlign: 'center', justifyContent: 'center' }}>
+  <h1 className="justify-content-md-center"> Hospital Dashboard </h1>
+{datas.map((item) => (
+<Alert variant="danger" className="justify-content-md-center">
+  <Alert.Heading className="justify-content-md-center">Incoming Patient Details</Alert.Heading>
+   
+    <div style={{ color: 'black',fontWeight : 'bold'}}>
+  <p>Name : {item.name}</p>
+    <p>Contact Number : {item.number}</p>
+    <p>Emergency Contact Number : {item.enumber}</p>
+    <p>Monitor Reading : {item.readings}</p>
+    <p>Nature of Emergency : {item.nature}</p>
+    <p>Severity of Emergency : {item.condition}</p>
+    </div>
+    
   <hr />
+  
   <p className="mb-0">
   <Button disabled variant="success" onClick={Accepted}><i class="fa fa-check ">  Accept</i></Button>
   <Button disabled variant="danger" onClick={Declined}><i class="fa fa-times">  Decline</i> </Button>
-  </p>
+  </p>  <br /> 
 </Alert>
+))}
     
 </div>
   );
