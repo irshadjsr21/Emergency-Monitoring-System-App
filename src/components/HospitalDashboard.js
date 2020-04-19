9a0910d9d8f860311db2433df7d8865b832ba22d
Harshak777 <Harshak777@users.noreply.github.com>
2020-04-19 21:01:13 +0530

    check the working of map

--- src/components/HospitalDashboard.js
+++ src/components/HospitalDashboard.js
@@ -2,16 +2,20 @@ import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
 
 
+const medical = datas.map(name=>{
+  console.log(name)
+})
 
 let socket;
 
 const HospitalDashboard = () => {
-  const [name, setName] = useState('');
+  /*const [name, setName] = useState('');
   const [number, setNumber] = useState('');
   const [enumber, setEnumber] = useState('');
   const [readings, setReadings] = useState('');
   const [nature, setNature] = useState('');
-  const [condition, setCondition] = useState('');
+  const [condition, setCondition] = useState('');*/
+  const [datas,setDatas] = useState([]);
 
   const ENDPOINT = 'http://localhost:4000';
 
@@ -23,25 +27,31 @@ const HospitalDashboard = () => {
   
   useEffect(() => {
     socket.on('chat', data => {
-      setName(data.name);
-      setNumber(data.number);
-      setEnumber(data.enumber);
-      setReadings(data.readings);
-      setNature(data.nature);
-      setCondition(data.condition);
+      console.log(data)
+      setDatas(datas=>[...datas,data])
     });
 }, []);
 
 
   return (
 <div>
-    <p>{name}</p>
-    <p>{number}</p>
-    <p>{enumber}</p>
-    <p>{readings}</p>
-    <p>{nature}</p>
-    <p>{condition}</p>
-</div>
+        <h3>Logged Exercises</h3>
+        <table className="table">
+          <thead className="thead-light">
+            <tr>
+              <th>name</th>
+              <th>number</th>
+              <th>enumber</th>
+              <th>readings</th>
+              <th>nature</th>
+              <th>condition</th>
+            </tr>
+          </thead>
+          <tbody>
+            {medical(datas)}
+          </tbody>
+        </table>
+      </div>
   );
 }
 
